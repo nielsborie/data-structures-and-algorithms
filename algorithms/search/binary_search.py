7694ae92fcc2f87e528cc095c21efb094420d0ef
nielsborie <nielsborie@gmail.com>
2023-10-30 23:03:42 +0100

    feat(search): implement the binary_search iterative version

--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -1,5 +1,5 @@
 # Recursive version
-def binary_search_r(arr: list[int], left:int, right: int, target: int):
+def binary_search_r(arr: list[int], left:int, right: int, target: int) -> int:
     if left >= right:
         return -1
     else:
@@ -11,16 +11,29 @@ def binary_search_r(arr: list[int], left:int, right: int, target: int):
         else:
             return binary_search_r(arr=arr, left=middle+1, right=right, target=target)
 
-def binary_search_v1(arr: list[int], target: int):
+def binary_search_v1(arr: list[int], target: int) -> int:
     return binary_search_r(arr=arr, target=target, left=0, right=len(arr)-1)
 
+# Iterative
+def binary_search_it(arr: list[int], target: int) -> int:
+    left_idx = 0
+    right_idx = len(arr)-1
+    while left_idx < right_idx:
+        middle_idx = left_idx + (right_idx - left_idx)//2
+        if arr[middle_idx] == target:
+            return middle_idx
+        elif arr[middle_idx] < target:
+            left_idx = middle_idx + 1
+        else:
+            right_idx = middle_idx
+    return -1
 
 if __name__== "__main__":
     input_arr = [1, 2, 5, 8, 9]
     target = 2
     print(f"Input array : {input_arr}")
     print(f"Searching the target value : {target}")
-    output = binary_search_v1(arr=input_arr, target=target)
+    output = binary_search_it(arr=input_arr, target=target)
     expected = 1
     assert output == expected, f"Expected {expected}, actual {output}"
-    print(f"Founded in index : {target}")
+    print(f"Founded in index : {output}")
