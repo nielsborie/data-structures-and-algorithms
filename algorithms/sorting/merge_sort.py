fbe2f16e30e1262302541a64251146fc88f49cb0
nielsborie <nielsborie@gmail.com>
2023-11-11 00:33:11 +0100

    feat: implement mergeSort algorithm

--- algorithms/sorting/merge_sort.py
+++ algorithms/sorting/merge_sort.py
@@ -1,3 +1,87 @@
+from abc import ABC, abstractmethod
+
+class AbstractMergeSort(ABC):
+  
+  @abstractmethod
+  def merge_sort(self, arr: list[int]) -> list[int]:
+    pass
+
+
+class MergeSortWithReplacement(AbstractMergeSort):
+  def merge_sort(self, arr: list[int]) -> list[int]:
+    self.merge_sort_helper(arr=arr, left_idx=0, right_idx=len(arr)-1)
+  
+  def mergeback(self, arr: list[int], start_left: int, start_right: int, end_right: int) -> list[int]:
+    tmp_arr = [0] * (end_right - start_left + 1)
+    left_pointer = start_left
+    right_pointer = start_right
+    # TODO: check edge cases -> what if there is no more element in left or right side 
+    for k in range(len(tmp_arr)):
+      if right_pointer > end_right or (left_pointer < start_right and arr[left_pointer] <= arr[right_pointer]):
+        tmp_arr[k] = arr[left_pointer]
+        left_pointer += 1
+      else:
+        tmp_arr[k] = arr[right_pointer]
+        right_pointer += 1
+    return tmp_arr
+  
+  def copyback(self, arr: list[int], tmp_arr: list[int], offset: int) -> None:
+    for k in range(len(tmp_arr)):
+      arr[k+offset] = tmp_arr[k]
+
+  def merge_sort_helper(self, arr: list[int], left_idx: int, right_idx: int) -> None:
+
+    if left_idx >= right_idx:
+      return 
+    
+    middle_idx = left_idx + (right_idx - left_idx) // 2
+    self.merge_sort_helper(arr=arr, left_idx=left_idx, right_idx=middle_idx)
+    self.merge_sort_helper(arr=arr, left_idx=middle_idx + 1, right_idx=right_idx)
+    tmp_arr = self.mergeback(arr=arr, start_left=left_idx, start_right=middle_idx+1, end_right=right_idx)
+    self.copyback(arr=arr, tmp_arr=tmp_arr, offset=left_idx)
+  
+class MergeSortNoReplacement(AbstractMergeSort):
+  def merge_sort(self, arr: list[int]) -> list[int]:
+
+    if len(arr) <= 1:
+      return
+
+    middle = len(arr) // 2
+    left = arr[:middle]
+    right = arr[middle:]
+
+    self.merge_sort(arr=left)
+    self.merge_sort(arr=right)
+
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+      if left[i] < right[j]:
+        arr[k] = left[i]
+        i += 1
+      else:
+        arr[k] = right[j]
+        j += 1
+      k += 1
+    
+    while i < len(left):
+      arr[k] = left[i]
+      i += 1
+      k += 1
+    
+    while j < len(right):
+      arr[k] = right[j]
+      j += 1
+      k += 1
+
+
+
+
+
+
+
+
+
+
 
 
 ## Merge Sort pseudo code
---
+++ algorithms/tests/sorting/test_sorting.py
@@ -0,0 +1,60 @@
+import unittest
+from abc import ABC, abstractmethod
+
+from algorithms.sorting.merge_sort import AbstractMergeSort, MergeSortWithReplacement, MergeSortNoReplacement
+
+
+class AbstractMergeSortTest(ABC):
+
+    @abstractmethod
+    def create_merge_sort_instance(self) -> 'AbstractMergeSort':
+        pass
+
+    def test_empty_list(self):
+        merge_sort = self.create_merge_sort_instance()
+        arr = []
+        expected = []
+        merge_sort.merge_sort(arr)
+        self.assertEqual(arr, expected)
+
+    def test_sorted_list(self):
+        merge_sort = self.create_merge_sort_instance()
+        arr = [1, 2, 3, 4, 5]
+        expected = [1, 2, 3, 4, 5]
+        merge_sort.merge_sort(arr)
+        self.assertEqual(arr, expected)
+
+    def test_reverse_sorted_list(self):
+        merge_sort = self.create_merge_sort_instance()
+        arr = [5, 4, 3, 2, 1]
+        expected = [1, 2, 3, 4, 5]
+        merge_sort.merge_sort(arr)
+        self.assertEqual(arr, expected)
+
+    def test_duplicate_elements(self):
+        merge_sort = self.create_merge_sort_instance()
+        arr = [4, 2, 3, 2, 1]
+        expected = [1, 2, 2, 3, 4]
+        merge_sort.merge_sort(arr)
+        self.assertEqual(arr, expected)
+
+    def test_large_list(self):
+        merge_sort = self.create_merge_sort_instance()
+        arr = [5, 2, 7, 1, 8, 3, 9, 6, 4]
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        merge_sort.merge_sort(arr)
+        self.assertEqual(arr, expected)
+
+
+class MergeSortWithReplacementTest(AbstractMergeSortTest, unittest.TestCase):
+
+    def create_merge_sort_instance(self) -> 'AbstractMergeSort':
+        return MergeSortWithReplacement()
+
+class MergeSortWithoutReplacementTest(AbstractMergeSortTest, unittest.TestCase):
+
+    def create_merge_sort_instance(self) -> 'AbstractMergeSort':
+        return MergeSortNoReplacement()
+
+if __name__ == '__main__':
+    unittest.main()
