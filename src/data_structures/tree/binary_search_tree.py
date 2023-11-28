54b7f213c904b513c897add17e7681e75ea2d3d7
nielsborie <nielsborie@gmail.com>
2023-11-29 00:17:15 +0100

    feat(tree): compute the height of a Binary Tree (aka the maximum depth) in 3 ways

--- src/data_structures/tree/binary_search_tree.py
+++ src/data_structures/tree/binary_search_tree.py
@@ -1,4 +1,7 @@
 
+from collections import deque
+
+
 class Node:
     def __init__(self, value: int) -> None:
         self.left: Node = None
@@ -30,6 +33,21 @@ class Node:
                 return self.right.contains(value=value)
             else:
                 return False
+
+    def _get_max_depth(self):
+        """Find the max depth in that Tree
+
+        Time complexity : O(N)
+        Space complexity : O(N) at worst
+        """
+        if self.left and self.right:
+            return 1 + max(self.left._get_max_depth(), self.right._get_max_depth())
+        elif self.left:
+            return 1 + self.left._get_max_depth()
+        elif self.right:
+            return 1 + self.right._get_max_depth()
+        else:
+            return 1
 
     def print_in_order(self) -> None:
         # left, self, right
@@ -70,7 +88,67 @@ class BinarySearchTree:
             return self.root.contains(value=value)
         else: 
             return False
+    
+    def get_height_recursive(self):
+        """Get the height of that Tree
+
+        Implementation using recursion.
+
+        Time complexity : O(N)
+        Space complexity : O(N) at worst
+        """
+        if self.root:
+            return self.root._get_max_depth()
+        else:
+            return 0
+
+    def get_height_breadth_first_search(self):
+        """Get the height of that Tree
+
+        Implementation using BFS.
+
+        Time complexity : O(N)
+        Space complexity : O(N) at worst
+        """
+        if not self.root:
+            return 0
         
+        level = 0
+        q = deque([self.root])
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
+    
+
+    def get_height_depth_first_search(self):
+        """Get the height of that Tree
+
+        Implementation using DFS.
+
+        Time complexity : O(N)
+        Space complexity : O(N) at worst
+        """
+        if not self.root:
+            return 0
+        
+        level = 1
+        stack = [(self.root, 1)]
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                level = max(level, depth)
+                stack.append((node.left, depth+1))
+                stack.append((node.right, depth+1))
+        return level
+
+
+    
     def print_in_order(self) -> None:
         if self.root:
             self.root.print_in_order()
@@ -97,3 +175,6 @@ if __name__ == "__main__":
     bst.print_post_order()
     print("Pre order :")
     bst.print_pre_order()
+    print(f"Height of the Tree [recursive version] : {bst.get_height_recursive()=}")
+    print(f"Height of the Tree [BFS version]: {bst.get_height_breadth_first_search()=}")
+    print(f"Height of the Tree [DFS version]: {bst.get_height_depth_first_search()=}")
