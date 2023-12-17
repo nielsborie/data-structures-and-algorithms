bb9ca38c245889362ef73b11fc236e9161224563
nielsborie <nielsborie@gmail.com>
2023-12-17 23:41:57 +0100

    test: add unit tests for avl tree

---
+++ tests/data_structures/tree/test_avl_tree.py
@@ -0,0 +1,41 @@
+import unittest
+
+from src.data_structures.tree.avl_tree import AVLTree
+
+class TestAVLTree(unittest.TestCase):
+    def setUp(self):
+        self.myTree = AVLTree()
+        self.root = None
+
+    def test_subtree_balance_after_insertion(self):
+        # Given an empty AVL tree
+        values = [10, 20, 30, 40, 50, 25]
+
+        # When inserting values into the AVL tree
+        for val in values:
+            self.root = self.myTree.insert(self.root, val)
+
+        # Then all subtrees in the AVL tree should be balanced
+        self.assertTrue(self.isSubtreeBalanced(self.root))
+
+    def isSubtreeBalanced(self, root):
+        # Helper function to check if a subtree is balanced
+
+        # Base case: an empty subtree is balanced
+        if root is None:
+            return True
+
+        # Check balance factor of the current node
+        balance_factor = self.myTree.get_balance(root)
+        if abs(balance_factor) > 1:
+            return False
+
+        # Recursively check balance of left and right subtrees
+        left_balanced = self.isSubtreeBalanced(root.left)
+        right_balanced = self.isSubtreeBalanced(root.right)
+
+        # The subtree is balanced if both left and right subtrees are balanced
+        return left_balanced and right_balanced
+
+if __name__ == '__main__':
+    unittest.main()
