00ecdb6128d4c2f69908609d588b1d668d085a66
nielsborie <nielsborie@gmail.com>
2023-12-22 23:58:42 +0100

    test: add avl tree delete test case

--- tests/data_structures/tree/test_avl_tree.py
+++ tests/data_structures/tree/test_avl_tree.py
@@ -14,11 +14,26 @@ class TestAVLTree(unittest.TestCase):
         # When inserting values into the AVL tree
         for val in values:
             self.root = self.myTree.insert(self.root, val)
+            self.assertTrue(self.is_subtree_balanced(self.root))
 
         # Then all subtrees in the AVL tree should be balanced
-        self.assertTrue(self.isSubtreeBalanced(self.root))
+        self.assertTrue(self.is_subtree_balanced(self.root))
 
-    def isSubtreeBalanced(self, root):
+    def test_subtree_balance_after_deletion(self):
+        # Given an empty AVL tree
+        values = [10, 20, 30, 40, 50, 25]
+
+        # When inserting values into the AVL tree
+        for val in values:
+            self.root = self.myTree.insert(self.root, val)
+            self.assertTrue(self.is_subtree_balanced(self.root))
+        
+        for val in reversed(values):
+            self.root = self.myTree.delete(self.root, val)
+            self.assertTrue(self.is_subtree_balanced(self.root))
+        
+
+    def is_subtree_balanced(self, root):
         # Helper function to check if a subtree is balanced
 
         # Base case: an empty subtree is balanced
@@ -31,8 +46,8 @@ class TestAVLTree(unittest.TestCase):
             return False
 
         # Recursively check balance of left and right subtrees
-        left_balanced = self.isSubtreeBalanced(root.left)
-        right_balanced = self.isSubtreeBalanced(root.right)
+        left_balanced = self.is_subtree_balanced(root.left)
+        right_balanced = self.is_subtree_balanced(root.right)
 
         # The subtree is balanced if both left and right subtrees are balanced
         return left_balanced and right_balanced
