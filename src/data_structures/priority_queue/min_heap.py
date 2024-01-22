524820c05d1db244b05a2b7fe52393a6f6e9e05e
nielsborie <nielsborie@gmail.com>
2024-01-23 00:29:17 +0100

    feat(min_heap): fix parent indice and add update method

--- src/data_structures/priority_queue/min_heap.py
+++ src/data_structures/priority_queue/min_heap.py
@@ -13,7 +13,7 @@ class MinHeap:
         return self.get_right_child_index(parent_index=index) < len(self.array)
     
     def get_parent_index(self, child_index: int) -> int:
-        return (child_index - 2)//2
+        return (child_index - 1)//2
     
     def get_left_child_index(self, parent_index: int) -> int:
         return 2*parent_index + 1
@@ -30,49 +30,65 @@ class MinHeap:
     def right(self, index: int) -> int:
         return self.array[self.get_right_child_index(parent_index=index)]
 
-    def heapify_down(self) -> None:
-        index = 0
+    def heapify_down(self, index: int) -> None:
         while self.has_left_child_index(index=index):
             smaller_child = self.get_left_child_index(parent_index=index)
             if self.has_right_child_index(index=index) and self.right(index=index) < self.left(index=index):
                 smaller_child = self.get_right_child_index(parent_index=index)
             
-            if self.array[index] < self.array[smaller_child]:
+            if self.array[index] <= self.array[smaller_child]:
                 break
             else:
                 self.swap(index_1=index, index_2=smaller_child)
             index = smaller_child
         
     
-    def heapify_up(self) -> None:
-        index = len(self.array) - 1
-        while self.has_parent(index=index) and self.parent(index=index) > self.array[index]:
+    def heapify_up(self, index: int) -> None:
+        while self.has_parent(index=index) and self.parent(index=index) >= self.array[index]:
             self.swap(index_1=self.get_parent_index(child_index=index), index_2=index)
             index = self.get_parent_index(child_index=index)
 
-    def swap(self, index_1: int, index_2) -> None:
-        tmp = self.array[index_1]
-        self.array[index_1] = self.array[index_2]
-        self.array[index_2] = tmp
+    def swap(self, index_1: int, index_2: int) -> None:
+        self.array[index_1], self.array[index_2] = self.array[index_2], self.array[index_1]
 
     def peek(self) -> int:
         if not self.array:
-            raise Exception(f"Cannot peek a value, the array is empty !")
+            raise IndexError(f"Cannot peek a value, the array is empty !")
         return self.array[0]
         
     def poll(self) -> int:
         if not self.array:
-            raise Exception(f"Cannot poll a value, the array is empty !")
+            raise IndexError(f"Cannot poll a value, the array is empty !")
+        
         min_value = self.array[0]
-        self.array[0] = self.array[self.size - 1]
+        last_value = self.array.pop()
         self.size -= 1
-        self.heapify_down()
-        return min_value
 
+        if self.array:
+            self.array[0] = last_value
+            self.heapify_down(index=0)
+        return min_value
+    
     def insert(self, value: int) -> None:
         self.array.append(value)
         self.size += 1
-        self.heapify_up()
+        self.heapify_up(index=len(self.array) - 1)
+
+    def update_by_index(self, i: int, new: int):
+        if i >= self.size:
+            raise IndexError(f"You're trying to update a non existing index; received {i=} but the heap has only {self.size} elements.")
+        old = self.array[i]
+        self.array[i] = new
+        if new < old:
+            self.heapify_up(i)
+        else:
+            self.heapify_down(i)
+
+    def update(self, old: int, new: int):
+        if old in self.array:
+            self.update_by_index(self.array.index(old), new) 
+        else:
+            raise ValueError(f"Value {old=} not found in the heap")
 
 if __name__ == "__main__":
     # Exemple d'utilisation
