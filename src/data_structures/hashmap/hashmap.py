630c12ec7071b379e913962771d13110e263bce9
nielsborie <nielsborie@gmail.com>
2024-03-17 17:39:53 +0100

    feat: improve HashMap implementation

--- src/data_structures/hashmap/hashmap.py
+++ src/data_structures/hashmap/hashmap.py
@@ -1,19 +1,20 @@
 class ListNode:
-    def __init__(self, key: int = -1, val: int = -1, next: 'ListNode' = None) -> None:
+    def __init__(self, key: int = -1, val: int = -1) -> None:
         self.val = val
         self.key = key
-        self.next = next
-
+        self.next = None
 
 class HashMap:
     def __init__(self, MAX_SIZE: int = 1000) -> None:
-        self.map: list[ListNode] = [ListNode() for _ in range(MAX_SIZE)]
+        self.MAX_SIZE = MAX_SIZE
+        self.hashmap: list[ListNode] = [ListNode() for _ in range(self.MAX_SIZE)]
 
     def hash(self, key: int) -> int:
-        return key % len(self.map)
+        return key % len(self.hashmap)
 
     def put(self, key: int, value: int) -> None:
-        cur = self.map[self.hash(key)]
+        idx = self.hash(key)
+        cur = self.hashmap[idx]
         while cur.next:
             if cur.next.key == key:
                 cur.next.val = value
@@ -22,7 +23,7 @@ class HashMap:
         cur.next = ListNode(key, value)
 
     def get(self, key: int) -> int:
-        cur = self.map[self.hash(key)].next
+        cur = self.hashmap[self.hash(key)].next
         while cur:
             if cur.key == key:
                 return cur.val
@@ -30,8 +31,9 @@ class HashMap:
         return -1
 
     def delete(self, key: int) -> None:
-        cur = self.map[self.hash(key)]
-        while cur and cur.next:
+        idx = self.hash(key)
+        cur = self.hashmap[idx]
+        while cur.next:
             if cur.next.key == key:
                 cur.next = cur.next.next
                 return
@@ -41,4 +43,9 @@ class HashMap:
         return not self.get(key) == -1
 
     def get_size(self) -> int:
-        pass
+        size = 0
+        for node in self.hashmap:
+            while node.next:
+                size += 1
+                node = node.next
+        return size
