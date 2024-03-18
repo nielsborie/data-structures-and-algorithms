ac54f37be86d8fc421215396e9865c68009990a3
nielsborie <nielsborie@gmail.com>
2024-03-18 23:17:53 +0100

    test: add unit test on HashMap implementation

--- tests/data_structures/hasmap/test_hasmap.py
+++ tests/data_structures/hasmap/test_hasmap.py
@@ -19,6 +19,48 @@ class TestHashMap(unittest.TestCase):
         self.assertEqual(hash_map.get(12), 45)
         self.assertEqual(hash_map.get(11), -1)
 
+    def test_delete(self):
+        # Given
+        hash_map = HashMap()
+        hash_map.put(1, 1)
+        hash_map.put(1000, 2)
 
-if __name__ == '__main__':
-    unittest.main()
+        # When
+        hash_map.delete(1)
+
+        # Then
+        self.assertEqual(hash_map.get(1), -1)
+        self.assertEqual(hash_map.get(1000), 2)
+
+    def test_contains(self):
+        # Given
+        hash_map = HashMap()
+        hash_map.put(1, 1)
+
+        # Then
+        self.assertTrue(hash_map.contains(1))
+        self.assertFalse(hash_map.contains(2))
+
+    def test_get_size_with_single_value(self):
+        # Given
+        hash_map = HashMap()
+        
+        # When
+        hash_map.put(1, 1)
+        
+        # Then
+        self.assertEqual(hash_map.get_size(), 1)
+        
+    def test_get_size(self):
+        # Given
+        hash_map = HashMap()
+        
+        # When
+        hash_map.put(1, 1)
+        hash_map.put(2, 1)
+        hash_map.put(3, 1)
+        hash_map.put(4, 1)
+        hash_map.put(5, 1)
+        
+        # Then
+        self.assertEqual(hash_map.get_size(), 5)
