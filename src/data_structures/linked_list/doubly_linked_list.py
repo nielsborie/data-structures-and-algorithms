e223c8d7da4abe940a55fd979ac76797683c3379
nielsborie <nielsborie@gmail.com>
2024-03-24 11:10:51 +0100

    feat: init DoublyLinkedList interface

---
+++ src/data_structures/linked_list/doubly_linked_list.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        # Insert a new element at the end of the list
+        pass
+
+    def insert_at_beginning(self, data):
+        # Insert a new element at the beginning of the list
+        pass
+
+    def insert_at_position(self, position, data):
+        # Insert a new element at a specific position in the list
+        pass
+
+    def delete_at_end(self):
+        # Delete the last element of the list
+        pass
+
+    def delete_at_beginning(self):
+        # Delete the first element of the list
+        pass
+
+    def delete_at_position(self, position):
+        # Delete the element at a given position in the list
+        pass
+
+    def display_forward(self):
+        # Display the elements of the list starting from the beginning
+        pass
+
+    def display_backward(self):
+        # Display the elements of the list starting from the end
+        pass
